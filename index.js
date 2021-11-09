c5da19ebb09c2e944ac3542e31796ba879d8db8e
samkabir <samkabir26@gmail.com>
2021-11-09 17:55:41 +0600

    Added DashBoard server side

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 
 //Middelware
 app.use(cors());
+app.use(express.json());
 
 //Connecting to Database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qmw5x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -15,9 +16,31 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         await client.connect();
+        const database = client.db('doctors_portal');
+        const appointmentsCollection = database.collection('appointments');
+
+
+        app.get('/appointments', async (req, res) => {
+          const email = req.query.email;
+          const date = new Date(req.query.date).toLocaleDateString();
+
+          const query = { email: email, date: date }
+
+          const cursor = appointmentsCollection.find(query);
+          const appointments = await cursor.toArray();
+          res.json(appointments);
+      })
+
+        app.post('/appointments', async (req, res) => {
+            const appopintment = req.body;
+            const result = await appointmentsCollection.insertOne(appopintment);
+            console.log(result);
+            res.json(result);
+        });
+
     }
     finally{
-
+        // await client.closes()
     }
 }
 run().catch(console.dir);
